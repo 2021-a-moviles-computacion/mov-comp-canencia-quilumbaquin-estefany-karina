c32b53ec839068a36cfb5ccf5fb9e2d378d68288
Estefany Canencia <estefany.canencia@epn.edu.ec>
2021-06-22 22:20:18 -0500

    Aplicativo, Layouts

---
+++ Moviles01/app/src/main/java/com/example/moviles01/AcicloVida.kt
@@ -0,0 +1,11 @@
+package com.example.moviles01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+}
--- Moviles01/app/src/main/java/com/example/moviles01/MainActivity.kt
+++ Moviles01/app/src/main/java/com/example/moviles01/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.moviles01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //this.findViewbyId()..Clase
+        val botenIrCicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+        botenIrCicloVida
+            .setOnClickListener {
+                abrirCicloVida()
+            }
+    }
+
+    fun abrirCicloVida(){
+        val intentExplicito= Intent( //intent es una calse, empieza con una laetra mayuscula
+            this,
+            AcicloVida::class.java
+        )
+        startActivity(intentExplicito)
     }
 }
