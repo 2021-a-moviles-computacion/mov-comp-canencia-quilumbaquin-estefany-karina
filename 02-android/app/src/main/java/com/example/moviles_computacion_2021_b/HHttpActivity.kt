e016d4b26730e3e0d3cceb808299258261d4daad
Estefany Canencia <estefany.canencia@epn.edu.ec>
2021-07-19 13:04:01 -0500

    Método Get - HHttpActivity

---
+++ 02-android/app/src/main/java/com/example/moviles_computacion_2021_b/HHttpActivity.kt
@@ -0,0 +1,32 @@
+package com.example.moviles_computacion_2021_b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+class HHttpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hhttp)
+
+        metodoGet()
+    }
+
+    fun metodoGet(){
+        "http://jsonplaceholder.typicode.com/posts/1"
+            .httpGet()
+            .responseString{req, res, result->
+                when(result){
+                    is Result.Failure -> {
+                        val error = result.getException()
+                        Log.i("http-klaxon","Error: ${error}")}
+                        is Result.Success ->{
+                            val getString = result.get()
+                            Log.i("http-klaxon", "${getString}")
+
+                    }
+                }
+            }
+    }
+}
