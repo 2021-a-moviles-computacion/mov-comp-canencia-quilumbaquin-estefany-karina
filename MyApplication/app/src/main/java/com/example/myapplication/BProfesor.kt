965a96754157df83f7afd9b1b9d5874a20c2c12d
Estefany Canencia <85120312+estefany-kari@users.noreply.github.com>
2021-07-23 17:51:40 -0500

    Delete MyApplication directory

--- MyApplication/app/src/main/java/com/example/myapplication/BProfesor.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-class BProfesor (val nombre: String,
-                 val materia: String,
-                 val edad: Int,
-                 val estadoCivil: Char,
-                 val telefono: String
-){
-}
