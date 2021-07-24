5853c7a842e604861903e6b6476ef44978e04072
Estefany Canencia <estefany.canencia@epn.edu.ec>
2021-07-23 21:08:02 -0500

    Proyecto - Login, Registrar

--- ProyectoG2/app/src/main/java/com/example/proyectog2/MainActivity.kt
+++ ProyectoG2/app/src/main/java/com/example/proyectog2/MainActivity.kt
@@ -4,15 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textViewRegistrarse = findViewById<TextView>(R.id.txt_registrarse)
 
-        val boton = findViewById<Button>(R.id.bntFecha)
-        boton
-            .setOnClickListener { abrirActividad(PruebaFechaHora::class.java) }
+        textViewRegistrarse.setOnClickListener { abrirActividad(registrarUsuario::class.java) }
+
+
     }
 
     fun abrirActividad(clase: Class<*>){
