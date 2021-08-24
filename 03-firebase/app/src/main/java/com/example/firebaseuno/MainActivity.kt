99585c9022b454b1c0704599567f58bcd91aedae
Estefany Canencia <estefany.canencia@epn.edu.ec>
2021-08-24 12:55:23 -0500

    firestore

--- 03-firebase/app/src/main/java/com/example/firebaseuno/MainActivity.kt
+++ 03-firebase/app/src/main/java/com/example/firebaseuno/MainActivity.kt
@@ -8,6 +8,9 @@ import android.util.Log
 import android.widget.Button
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
@@ -46,16 +49,46 @@ class MainActivity : AppCompatActivity() {
         when (requestCode){
             CODIGO_INICIO_SESION ->{
                 if(resultCode== Activity.RESULT_OK){
-                    val usuario = IdpResponse.fromResultIntent(data)
-                    if(usuario?.isNewUser == true){
+                    val usuario : IdpResponse? = IdpResponse.fromResultIntent(data)
+                    if(usuario != null){
+                     if(usuario?.isNewUser == true){
                         Log.i("firebase-login","Nuevo Usuario")
-                    }else{
+                        registrarUsuarioPorPrimeraVez(usuario)
+                     }else{
                         Log.i("firebase-login","Usuario Antiguo")
-                    }
+                    }}
                 }else{
                     Log.i("firebase-login","El usuario canceló")
                 }
             }
+        }
+    }
+
+    fun registrarUsuarioPorPrimeraVez(usuario: IdpResponse){
+        val usuarioLogeado = FirebaseAuth.getInstance().getCurrentUser()
+        if(usuario.email != null && usuarioLogeado !=null){
+            //roles: ["usuairo", "admin"]
+            val db = Firebase.firestore
+            val rolesUsuario = arrayListOf("usuario")
+            val nuevoUsuario = hashMapOf<String, Any>(
+                "roles" to rolesUsuario,
+                "uid" to usuarioLogeado.uid
+            )
+            val identificadorUsuario = usuario.email
+            db.collection("usuario")
+                //.add(nuevoUsuario)
+                    // forma a: Firestore crea el identificado
+                    //forma b: yo seteo el identificado
+                .document(identificadorUsuario.toString())
+                .set(nuevoUsuario)
+                .addOnSuccessListener {
+                    Log.i("firebase-firestore","Se creó")
+                }
+                .addOnFailureListener{
+                    Log.i("firebase - firestore", "falló")
+                }
+        }else{
+            Log.i("firebase-login", "ERROR")
         }
     }
 }
